5a06a6a06ea25a002dbe21dcfee21db8f7adaca2
Kostya <kosmat3101@gmail.com>
2022-08-29 19:13:04 +0300

    statisticts styled

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,32 @@
 import { Profile } from "../Profile/Profile";
+import { Statistics } from "../Statistics/Statistics"
 import user from "../Profile/user";
+import data from "../Statistics/data.json"
 export const App = () => {
   const { userName, tag, location, avatar, stats } = user;
   return (
+    <div
+      style={{
+        
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+        flexDirection: 'column'
+      }}
+      >
     <Profile
     userName={userName}
     tag={tag}
-      location={location}
-      avatar={avatar}
-      stats={stats} />
+    location={location}
+    avatar={avatar}
+    stats={stats} />
     
+    <Statistics 
+    titel="Upload stats"
+    stats={data} 
+      />
+    </div>
   );
 };
